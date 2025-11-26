6a91008a1fdc959e248f7a8ce5886965dc5e8683
xuejmnet <326308290@qq.com>
2025-11-26 12:37:41 +0800

    Update MDSApplication.java

--- src/main/java/com/eq/mds/MDSApplication.java
+++ src/main/java/com/eq/mds/MDSApplication.java
@@ -3,6 +3,7 @@ package com.eq.mds;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * create time 2025/11/17 15:21
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @author xuejiaming
  */
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableTransactionManagement
 public class MDSApplication {
     public static void main(String[] args) {
         SpringApplication.run(MDSApplication.class, args);
